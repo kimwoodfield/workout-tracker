472955d4cce08bc4fc188520b393efe0e91206d0
kimwoodfield <kimwoodfield@live.com>
2021-05-17 13:27:22 +1000

    settings updated

--- frontend/pages/settings.js
+++ frontend/pages/settings.js
@@ -15,6 +15,10 @@ import "react-confirm-alert/src/react-confirm-alert.css";
 import { useRouter } from "next/router";
 import Link from "next/link";
 
+const PageWrapper = styled.div`
+  font-family: Roboto, sans-serif;
+`;
+
 const Padding = styled.div`
   height: 4rem;
 `;
@@ -25,6 +29,7 @@ const ButtonContainer = styled.div`
   width: 90%;
   align-items: center;
   color: grey;
+  margin: 0 auto;
 
   ButtonContainer:nth-child(2) {
     background: red;
@@ -32,8 +37,6 @@ const ButtonContainer = styled.div`
 `;
 
 const Button = styled.button`
-  // border: 1px solid lightgray;
-  // border-radius: 5px;
   background-color: ${({ theme }) => theme.navbar};
   color: ${({ theme }) => theme.text};
   font-size: 1.15em;
@@ -43,7 +46,6 @@ const Button = styled.button`
   padding: 0;
   text-align: left;
   padding-left: 0.75rem;
-  // margin: 0.25rem 0;
 
   &:nth-of-type(1) {
     border-bottom: none;
@@ -185,7 +187,7 @@ export default function Settings() {
   };
 
   return (
-    <div className="container">
+    <PageWrapper>
       <Head>
         <title>Settings</title>
         <link rel="icon" href="/favicon.ico" />
@@ -232,159 +234,7 @@ export default function Settings() {
         </button>
       </ButtonContainer>
 
-      <main>
-        <div></div>
-      </main>
-
       <IconNavBar />
-
-      <style jsx>{`
-        .container {
-          // background-color: #fafafa;
-          min-height: 100vh;
-          // padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          max-width: 100%;
-        }
-
-        main {
-          // padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+    </PageWrapper>
   );
 }
